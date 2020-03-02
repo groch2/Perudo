733282da4b9f95202901731055c52950018d5680
Roch Deschaseaux <rdeschaseaux@talentsoft.com>
2020-03-02 11:41:39 +0100

    return the game state from playerPlays function

--- PerudoGame.ts
+++ PerudoGame.ts
@@ -1,28 +1,33 @@
 export namespace PerudoGame {
 	export enum DiceFace { Paco, Two, Three, Four, Five, Six }
 
-	export type PlayerDiceBid = { diceFace: DiceFace, diceQuantity: Number }
-	
+	export type PlayerDiceBid = { diceFace: DiceFace, diceQuantity: number }
+
 	export enum PlayerEndOfTurnCall { Bluff, ExactMatch }
 
 	export class Game {
 		constructor(
-			readonly playersDices: Number[],
-			playerTurn: Number,
-			lastBid: PlayerDiceBid | undefined) { }
+			readonly turn: number,
+			readonly playersDices: number[],
+			readonly playerTurn: number,
+			readonly lastBid: PlayerDiceBid | undefined) { }
 	}
-	
+
 	function isDiceBid(playerChoice: PlayerDiceBid | PlayerEndOfTurnCall): playerChoice is PlayerDiceBid {
 		return (playerChoice as PlayerDiceBid).diceFace !== undefined;
 	}
 
-	export function playerPlays(playerPosition: Number, playerChoice: PlayerDiceBid | PlayerEndOfTurnCall): Game {
+	export function playerPlays(game: Game, playerPosition: number, playerChoice: PlayerDiceBid | PlayerEndOfTurnCall): Game {
 		if (isDiceBid(playerChoice)) {
 			console.log(playerChoice);
+			return (
+				new Game(
+					game.turn + 1,
+					game.playersDices,
+					game.playerTurn + 1,
+					<PlayerDiceBid>playerChoice));
 		}
-		else {
-			console.log(PlayerEndOfTurnCall[<PlayerEndOfTurnCall>playerChoice]);
-		}
-		return;
+		console.log(PlayerEndOfTurnCall[<PlayerEndOfTurnCall>playerChoice]);
+		return game;
 	}
 }
--- Test.ts
+++ Test.ts
@@ -1,5 +1,13 @@
 import { PerudoGame } from "./PerudoGame";
 
-PerudoGame.playerPlays(0, { diceFace: PerudoGame.DiceFace.Paco, diceQuantity: 3 });
+const nbPlayers = 6;
+const playersDices: number[] = Object.assign([], { length: nbPlayers }).fill(5);
 
-PerudoGame.playerPlays(1, PerudoGame.PlayerEndOfTurnCall.Bluff);
+const game = new PerudoGame.Game(0, playersDices, 0, undefined);
+let testGame: PerudoGame.Game;
+
+testGame = PerudoGame.playerPlays(game, 0, { diceFace: PerudoGame.DiceFace.Paco, diceQuantity: 3 });
+console.log(testGame);
+
+testGame = PerudoGame.playerPlays(game, 1, PerudoGame.PlayerEndOfTurnCall.Bluff);
+console.log(testGame);
