13f8a0aa8a65f305e0c87cd345e1866ef3751acb
Roch Deschaseaux <rdeschaseaux@talentsoft.com>
2020-05-08 12:21:51 +0200

    Improve tests

--- test/GetTheTotalNumberOfDicesByFace.ts
+++ test/GetTheTotalNumberOfDicesByFace.ts
@@ -3,5 +3,5 @@ import { PerudoGame } from "../PerudoGame";
 const nbPlayers = 3;
 const game = new PerudoGame.Game(nbPlayers);
 console.log(game.currentRound.playersDicesDrawByPlayerId);
-const allDices = game.currentRound.getTotalNbDiceByFaceName();
+const allDices = game.currentRound.getTotalPositiveNbDiceByFaceName();
 console.log({ allDices });
--- test/ThrowDices.ts
+++ test/ThrowDices.ts
@@ -1,5 +1,4 @@
 import { PerudoGame } from "../PerudoGame";
 
 let diceDraw = PerudoGame.getDrawByThrowingDices(5);
-//[...diceDraw.entries()].map(pair => [PerudoGame.DiceFace[pair[0]], pair[1]]);
-console.log([...diceDraw.entries()]);
+console.log([...diceDraw.entries()].filter(([, quantity]) => quantity > 0));
