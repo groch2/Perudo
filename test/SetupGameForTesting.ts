d363aa80bb1288c27c39d98ce3daf65f4ab0efd5
Roch Deschaseaux <rdeschaseaux@talentsoft.com>
2020-05-14 10:34:50 +0200

    Add a visual test for the setting of a game.

---
+++ test/SetupGameForTesting.ts
@@ -0,0 +1,16 @@
+import { PerudoGame } from "../PerudoGame";
+
+const nbPlayers = 2;
+PerudoGame.throwingDicesEnabled = false;
+
+(function secondPlayerLoosesTheRoundAndIsOutOfDice() {
+    const game = new PerudoGame.Game(nbPlayers);
+
+    game.currentRound.playersDicesDrawByPlayerId[0].set(<any>PerudoGame.DiceFace[PerudoGame.DiceFace.Two], 2);
+    game.currentRound.playersDicesDrawByPlayerId[0].set(<any>PerudoGame.DiceFace[PerudoGame.DiceFace.Three], 3);
+
+    game.currentRound.playersDicesDrawByPlayerId[1].set(<any>PerudoGame.DiceFace[PerudoGame.DiceFace.Four], 3);
+    game.currentRound.playersDicesDrawByPlayerId[1].set(<any>PerudoGame.DiceFace[PerudoGame.DiceFace.Five], 2);
+
+    console.log(game.getDicesFacesOfCurrentRoundByPlayerId());
+})();
