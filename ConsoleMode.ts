66c54200f0c83d472144c9baf5353d9831b0788a
Roch Deschaseaux <rdeschaseaux@talentsoft.com>
2020-05-26 01:00:30 +0200

    Fix the state of the console mode. There is a bug with the setting of the next player.

--- ConsoleMode.ts
+++ ConsoleMode.ts
@@ -16,28 +16,37 @@ const rl =
 
 class AskForBidOrEndOfRoundProcessor {
     public readonly question: string;
-    constructor(private game: PerudoGame.Game) {
-        this.question = `The current bid is: ${game.currentBidNbDices} ${PerudoGame.diceFacesNames[game.currentBidDiceFace]};
-Do you want to bid (1), call bluff (2) or end of round (3) ?`;
+    constructor(private game: PerudoGame.Game, readonly isPreviousChoiceValid: boolean = true) {
+        this.question = `${isPreviousChoiceValid ? "" : "Your input is incorrect, please choose a valid option.\n"}`;
+        this.question += this.game.currentRound.turnNumber > 0 ? `The current bid is: ${this.game.currentBidNbDices} ${PerudoGame.diceFacesNames[this.game.currentBidDiceFace]}\n` : "";
+        this.question += `Do you want to bid (1), call bluff (2) or exact match (3) ?`;
     }
-    processChoice(choice: string): AskForBidOrEndOfRoundProcessor | AskForBidDiceFaceProcessor {
+    processChoice(choice: string | number): AskForBidOrEndOfRoundProcessor | AskForDiceFaceProcessor {
+        choice = Number.parseInt(choice as string);
         switch (choice) {
-            case "1":
-                return new AskForBidDiceFaceProcessor(this.game);
-            case "2":
-                game.callBluff();
+            case 1:
+                return new AskForDiceFaceProcessor(this.game);
+            case 2:
+                this.game.callBluff();
                 break;
-            case "3":
-                game.callExactMatch();
+            case 3:
+                this.game.callExactMatch();
                 break;
         }
-        return this;
+        switch (choice) {
+            case 2:
+            case 3:
+                return new AskForDiceFaceProcessor(this.game);
+        }
+        return new AskForBidOrEndOfRoundProcessor(this.game, false);
     }
 }
 
-class AskForBidDiceFaceProcessor {
+class AskForDiceFaceProcessor {
     public readonly question: string;
-    constructor(private game: PerudoGame.Game) {
+    constructor(private game: PerudoGame.Game, readonly isPreviousChoiceValid: boolean = true) {
+        this.question = `${isPreviousChoiceValid ? "" : "Your input is incorrect, please choose a valid option.\n"}`;
+        this.question += this.game.currentRound.turnNumber > 0 ? `The current bid is: ${this.game.currentBidNbDices} ${PerudoGame.diceFacesNames[this.game.currentBidDiceFace]}\n` : "";
         let nextDiceFaceChoices: PerudoGame.DiceFace[] | string =
             [...new Array(PerudoGame.diceFacesNames.length).keys()].slice(1);
         if (this.game.currentRound.isFirstPlayerPalafico || this.game.currentRound.turnNumber > 0) {
@@ -45,32 +54,41 @@ class AskForBidDiceFaceProcessor {
             nextDiceFaceChoices.unshift(PerudoGame.DiceFace.Paco);
         }
         nextDiceFaceChoices = nextDiceFaceChoices.map(df => `${PerudoGame.diceFacesNames[df]}: ${df + 1}`).join(", ");
-        this.question = `On which dice face to you want to bid ? (${nextDiceFaceChoices})`;
+        this.question += `On which dice face to you want to bid ? (${nextDiceFaceChoices})`;
     }
-    processChoice(choice: string | number): AskForBidDiceQuantityProcessor | AskForBidOrEndOfRoundProcessor {
+    processChoice(choice: string | number): AskForDiceQuantityProcessor | AskForBidOrEndOfRoundProcessor | AskForDiceFaceProcessor {
         choice = Number.parseInt(choice as string) - 1;
         if (choice >= 0 && choice < 6) {
-            return new AskForBidDiceQuantityProcessor(this.game, choice);
+            return new AskForDiceQuantityProcessor(this.game, choice);
         }
-        // TODO: notifiy the user that his input is invalid and that he is redirected to the first choice (do you want to bid or end the round by calling bluff or exact match)
-        return new AskForBidOrEndOfRoundProcessor(this.game);
+        return new (
+            this.game.currentRound.isRoundBeginning ?
+                AskForDiceFaceProcessor :
+                AskForBidOrEndOfRoundProcessor)(this.game, false);
     }
 }
 
-class AskForBidDiceQuantityProcessor {
+class AskForDiceQuantityProcessor {
     public readonly question = `How many ${PerudoGame.DiceFace[this.diceFace]} do you want to bid ? (enter an integer numeric value)`;
-    constructor(private game: PerudoGame.Game, private diceFace: number) { }
-    processChoice(choice: string | number): AskForBidDiceQuantityProcessor | AskForBidOrEndOfRoundProcessor {
+    constructor(private game: PerudoGame.Game, private diceFace: PerudoGame.DiceFace) { }
+    processChoice(choice: string | number): AskForDiceQuantityProcessor | AskForBidOrEndOfRoundProcessor | AskForDiceFaceProcessor {
         // TODO: validate that the user input is correct (it must be an integer number less than the total number of dices that are still on the table)
         // TODO: catch the error thrown by playerPlays if the bid is invalid
         // TODO: return a new AskForBidOrEndOfRoundProcessor if the user input is incorrect, or if the bid is invalid (resest the turn to allow the current player to make another choice)
-        choice = Number.parseInt(choice as string);
-        game.bid(choice, this.diceFace as PerudoGame.DiceFace);
+        if (!/^\d+$/.test(choice as string)) {
+            return new (
+                this.game.currentRound.isRoundBeginning ?
+                    AskForDiceFaceProcessor :
+                    AskForBidOrEndOfRoundProcessor)(this.game, false);
+        }
+        console.debug({ playerBefore: this.game.nextPlayerId });
+        this.game.bid(choice as number, this.diceFace);
+        console.debug({ playerAfter: this.game.nextPlayerId });
         return new AskForBidOrEndOfRoundProcessor(this.game);
     }
 }
 
-(function loop(processor: AskForBidOrEndOfRoundProcessor | AskForBidDiceFaceProcessor | AskForBidDiceQuantityProcessor) {
+(function loop(processor: AskForBidOrEndOfRoundProcessor | AskForDiceFaceProcessor | AskForDiceQuantityProcessor) {
     const question =
         `round number: ${game.currentRoundNumber}
 player id: ${game.nextPlayerId}
@@ -91,4 +109,4 @@ ${processor.question}\n`;
             }
             loop(processor);
         });
-})(new AskForBidDiceFaceProcessor(game));
+})(new AskForDiceFaceProcessor(game));
